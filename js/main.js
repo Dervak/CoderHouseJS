f0144e0e444a19f0a7b8b5b38af131d0ba507b02
Dervak <federagone@gmail.com>
2022-07-20 23:50:45 -0300

    primer entrega

--- js/main.js
+++ js/main.js
@@ -1,42 +1,86 @@
 let name = ""
 let darkmode = false;
+let products = [
+    { name: "Papas Fritas", price: 200, store: "McBurger" },
+    { name: "Asado", price: 1200, store: "Parrilla 'El Rey Wilson' " },
+    { name: "Helado", price: 300, store: "Luccianos" },
+    { name: "Agua en un recital", price: 500, store: "Paisano" },
+    { name: "Sushi", price: 2200, store: "Izakaya" },
+]
+let productsaux = products;
+let productName = ""
 
-function welcome(name){
-    return name = `Bienvenido ${name}`
+function welcomeTitle(name) {
+    document.getElementById("title").innerText = `Bienvenido ${name} a este hermoso "carrito de compras" version alpha-0.01`
 }
 function darkModeToggler() {
-    if (!darkmode){
+    if (!darkmode) {
         document.getElementById("title").style.color = "#F0FFFF"
-        document.getElementById("results").style.background ="aquamarine"
+        document.getElementById("description").style.color = "#F0FFFF"
+        document.getElementById("products").style.background = "aquamarine"
         document.body.style.background = "#282828";
         document.getElementById("toggler").innerHTML = "Modo Claro"
         darkmode = true
     } else {
         document.getElementById("title").style.color = "black"
-        document.getElementById("results").style.background ="white"
+        document.getElementById("description").style.color = "black"
+        document.getElementById("products").style.background = "white"
         document.body.style.background = "white";
         document.getElementById("toggler").innerHTML = "Modo Oscuro"
         darkmode = false
     }
 
 }
-function addResult(num, i){
-    document.getElementById("results").innerHTML += `<div class="result-item">${num} x ${i} = ${num*i}</div>`
-}
-function multiplication(){
-    document.getElementById("results").innerHTML = ""
-    for (i = 1; i <= 10; i++){
-        addResult((i), document.getElementById("num").value)
+function addProduct(products) {
+    document.getElementById("products").innerHTML = ""
+    let i = 0
+    for (let product of products) {
+        document.getElementById("products").innerHTML += `<div class="product${i}"><div class="name">Producto: ${product.name}</div><div class="price"> Precio: $${product.price}</div><div class="store">Tienda: ${product.store}</div></div>`
+        i++;
     }
 }
-function nameChecker (name){
-    while (name === null || name === ""){
+function deleteProduct (name, products){
+    for (let product of products){
+        if (product.name === name){
+            products.splice(products.map(function(p){return p.name}).indexOf(product.name), 1)
+            addProduct(products)
+        }
+    }
+    productName = ""
+    document.getElementById("product-name").value = ""
+    return products
+}
+
+function searchProduct(name, products) {
+        if ((products.filter(product => product.name === name)).length > 0){
+            addProduct(products.filter(product => product.name === name))
+        }else{
+            document.getElementById("products").innerHTML = `No hay resultados con ${name}`
+        }
+        document.getElementById("search").innerText = "Restablecer"
+
+}
+function restoreSearch(products){
+    addProduct(products)
+    productName = ""
+    document.getElementById("product-name").value = ""
+    document.getElementById("search").innerText = "Buscar"
+}
+function nameChecker(name) {
+    while (name === null || name === "") {
         name = prompt("Ingresa tu nombre!")
     }
     return name
 }
-
-name = nameChecker(name)
-
-alert(welcome(name))
-
+addProduct(productsaux)
+document.getElementById("product-name").oninput = () => productName = document.getElementById("product-name").value
+welcomeTitle(nameChecker(name))
+document.getElementById("toggler").onclick = () => darkModeToggler()
+document.getElementById("search").onclick = () => { console.log("me clickeaste")
+    if (document.getElementById("search").innerText === "Buscar") {
+        searchProduct(productName, productsaux)
+    } else {
+        restoreSearch(productsaux)
+    }
+}
+document.getElementById("delete").onclick = () => productsaux = deleteProduct(productName, productsaux)
