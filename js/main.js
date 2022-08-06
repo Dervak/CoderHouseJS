e76a43d354d04a04a54d30a0854c078ac0a89dbb
Dervak <federagone@gmail.com>
2022-08-06 10:33:35 -0300

    shopping cart progress

--- js/main.js
+++ js/main.js
@@ -53,8 +53,10 @@ function addProduct(products) {
         let newProd = document.createElement("div")
         newProd.setAttribute("id", product.id)
         newProd.classList.add("card")
-        newProd.innerHTML = `<img src="./resources/images/${product.id}.png" alt="${product.name}"><div class="store" id="${product.store}">Tienda: ${product.store}</div><div id="${product.name}" class="name">Producto: ${product.name}</div><div class="price">Precio: $${product.price}</div>`
+        newProd.innerHTML = `<img src="./resources/images/${product.id}.png" alt="${product.name}"><div class="store" id="${product.store}">${product.store}</div><div id="${product.name}" class="name">${product.name}</div><div class="price">$${product.price}</div><div id="card-btns"><button class="card-buy button">Comprar</button></div>`
         document.getElementById("products").appendChild(newProd)
+        setTimeout(cardAdapter(), 500)
+        document.getElementsByClassName("card-buy")[i].onclick = (event) => buyProduct(event)
         i++;
     }
 }
@@ -67,19 +69,24 @@ function deleteProduct (name, products){
     }
     productName = ""
     document.getElementById("product-name").value = ""
-    document.getElementById("search").innerText === "Restablecer" && (document.getElementById("search").innerText = "Buscar")
+    document.getElementById("search").innerHTML === '<i class="fa-solid fa-rotate-left"></i>' && (document.getElementById("search").innerHTML = '<i class="fa-solid fa-magnifying-glass"></i>')
     return products
+}
+function cardAdapter(){
+    document.documentElement.style.setProperty("--card-pos", - (document.getElementsByClassName("card")[0].childNodes[0].offsetHeight/32) + "rem" )
+    document.documentElement.style.setProperty("--card-margin", ((document.getElementsByClassName("card")[0].childNodes[0].offsetHeight/32) + 0.5) + "rem" )
+    document.documentElement.style.setProperty("--card-height", ((document.getElementsByClassName("card")[0].offsetHeight/32)) + "rem" )
 }
 
 function searchProduct(name, products) {
     ((products.filter(product => product.name === name)).length > 0) ? addProduct(products.filter(product => product.name === name)) : document.getElementById("products").innerHTML = `No hay resultados con ${name}`;
-    document.getElementById("search").innerText = "Restablecer"
+    document.getElementById("search").innerHTML = '<i class="fa-solid fa-rotate-left"></i>'
 }
 function restoreSearch(products){
     addProduct(products)
     productName = ""
     document.getElementById("product-name").value = ""
-    document.getElementById("search").innerText = "Buscar"
+    document.getElementById("search").innerHTML = '<i class="fa-solid fa-magnifying-glass"></i>'
 }
 function nameChecker(name) {
     while (name === null || name === "") {
@@ -87,16 +94,39 @@ function nameChecker(name) {
     }
     return name
 }
+function cartPopUpHandler (){
+    if (document.getElementById("popup-bg")){
+        document.getElementById("popup-bg").classList.remove("active")
+        setTimeout(() => document.body.removeChild(document.getElementById("popup-bg")), 500)
+    }else{
+        let newEle = document.createElement("div")
+        newEle.classList.add("popup-bg")
+        newEle.setAttribute("id", newEle.classList)
+        document.body.appendChild(newEle)
+        setTimeout(() => document.getElementById("popup-bg").classList.add("active")) 
+    }
+    
+}
+function buyProduct(event){
+    console.log(event)
+    event.target.remove() 
+    
+}
 
 //CARGA
 window.onload = () => darkModeToggler()
 createProduct();
 addProduct(productsaux)
+
 welcomeTitle(name)
 
 
 //EVENTOS
 document.getElementById("product-name").oninput = () => productName = document.getElementById("product-name").value
 document.getElementById("toggler").onclick = () => darkModeToggler()
-document.getElementById("search").onclick = () => document.getElementById("search").innerText === "Buscar" ? searchProduct(productName, productsaux) : restoreSearch(productsaux)
+document.getElementById("sc-btn").onclick = () => cartPopUpHandler()
+new MutationObserver(() => document.getElementById("popup-bg") !== null && (document.getElementById("popup-bg").onclick = () => cartPopUpHandler())).observe(document.body, {childList: true}) //Soy un enfermo no habia razon para hacer esto, pero queria que sea dinámica la creacion del evento.
+document.getElementById("search").onclick = () => document.getElementById("search").innerHTML === '<i class="fa-solid fa-magnifying-glass"></i>' ? searchProduct(productName, productsaux) : restoreSearch(productsaux)
 document.getElementById("delete").onclick = () => productsaux = deleteProduct(productName, productsaux)
+setTimeout(() => cardAdapter(), 15)
+window.onresize = () => cardAdapter()
